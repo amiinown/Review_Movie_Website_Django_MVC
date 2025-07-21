7040ce162c7f3eab614fcdc3a9e0bfcddeb5cfe7
amin <salamon1384@gmail.com>
2025-07-21 16:50:29 +0330

    some change like ignore pycache and change information of the admin account.

--- accounts/admin.py
+++ accounts/admin.py
@@ -31,8 +31,8 @@ class UserAdmin(BaseUserAdmin):
     readonly_fields = ('date_joined', 'last_login')
     filter_horizontal = ('groups', 'user_permissions')
 
-    def get_form(self, request, obj = ..., change = ..., **kwargs):
-        form = super().get_form(request, obj, change, **kwargs)
+    def get_form(self, request, obj = None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
         is_superuser = request.user.is_superuser
         if not is_superuser:
             form.base_fields['is_superuser'].disabled = True
